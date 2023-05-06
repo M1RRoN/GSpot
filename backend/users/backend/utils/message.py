a4395cb106db4ddada47bdae4ff500600761f3d7
M1RRoN <mds.myst@gmail.com>
2023-05-06 11:40:04 +0400

    add rabbitmq settings and refactoring

--- backend/users/backend/utils/message.py
+++ backend/users/backend/utils/message.py
@@ -1,89 +1,42 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from utils.rabbitmq import RabbitMQ
 
 
 class BaseMessage(ABC):
-    @abstractmethod
-    def send(self):
-        pass
+    def __init__(self, message: str, exchange_name: str, queue_name: str) -> None:
+        self.message = message
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+
+    def send(self) -> None:
+        with RabbitMQ(
+                exchange_name=self.exchange_name,
+                queue_name=self.queue_name
+        ) as rabbitmq:
+            rabbitmq.publish(self.message)
 
 
 class DevActivationMessage(BaseMessage):
-    def __init__(self, message):
-        self.message = message
-
-    def send(self):
-        rabbitmq = RabbitMQ(
-            host='localhost',
-            port=5672,
-            username='guest',
-            password='guest',
-            exchange_name='dev_activation_exchange'
-        )
-        rabbitmq.publish(self.message)
-        rabbitmq.close()
+    def __init__(self, message: str) -> None:
+        super().__init__(message, 'dev_activation_exchange', 'dev_activation_queue')
 
 
 class ClientActivationMessage(BaseMessage):
-    def __init__(self, message):
-        self.message = message
-
-    def send(self):
-        rabbitmq = RabbitMQ(
-            host='localhost',
-            port=5672,
-            username='guest',
-            password='guest',
-            exchange_name='client_activation_exchange'
-        )
-        rabbitmq.publish(self.message)
-        rabbitmq.close()
+    def __init__(self, message: str) -> None:
+        super().__init__(message, 'client_activation_exchange', 'client_activation_queue')
 
 
 class OwnerAccessMessage(BaseMessage):
-    def __init__(self, message):
-        self.message = message
-
-    def send(self):
-        rabbitmq = RabbitMQ(
-            host='localhost',
-            port=5672,
-            username='guest',
-            password='guest',
-            exchange_name='owner_access_exchange'
-        )
-        rabbitmq.publish(self.message)
-        rabbitmq.close()
+    def __init__(self, message: str) -> None:
+        super().__init__(message, 'owner_access_exchange', 'owner_access_queue')
 
 
 class DevAccessMessage(BaseMessage):
-    def __init__(self, message):
-        self.message = message
-
-    def send(self):
-        rabbitmq = RabbitMQ(
-            host='localhost',
-            port=5672,
-            username='guest',
-            password='guest',
-            exchange_name='dev_access_exchange'
-        )
-        rabbitmq.publish(self.message)
-        rabbitmq.close()
+    def __init__(self, message: str) -> None:
+        super().__init__(message, 'dev_access_exchange', 'dev_access_queue')
 
 
 class FriendAddedMessage(BaseMessage):
-    def __init__(self, message):
-        self.message = message
-
-    def send(self):
-        rabbitmq = RabbitMQ(
-            host='localhost',
-            port=5672,
-            username='guest',
-            password='guest',
-            exchange_name='friend_added_exchange'
-        )
-        rabbitmq.publish(self.message)
-        rabbitmq.close()
+    def __init__(self, message: str) -> None:
+        super().__init__(message, 'friend_added_exchange', 'friend_added_queue')
--- backend/users/backend/utils/rabbitmq.py
+++ backend/users/backend/utils/rabbitmq.py
@@ -1,13 +1,16 @@
 import pika
 
+from config.settings.rabbitmq import RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USERNAME, RABBITMQ_PASSWORD
+
 
 class RabbitMQ:
-    def __init__(self, host, port, username, password, exchange_name):
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
+    def __init__(self, exchange_name, queue_name):
+        self.host = RABBITMQ_HOST
+        self.port = RABBITMQ_PORT
+        self.username = RABBITMQ_USERNAME
+        self.password = RABBITMQ_PASSWORD
         self.exchange_name = exchange_name
+        self.queue_name = queue_name
 
         credentials = pika.PlainCredentials(self.username, self.password)
         parameters = pika.ConnectionParameters(
@@ -18,14 +21,10 @@ class RabbitMQ:
         )
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='fanout')
 
-    def publish(self, message):
+    def publish(self, message: str) -> None:
         self.channel.basic_publish(
             exchange=self.exchange_name,
-            routing_key='',
-            body=message
+            routing_key=self.queue_name,
+            body=message.encode()
         )
-
-    def close(self):
-        self.connection.close()
