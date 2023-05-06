10ee4e02881c97f872fad22db7cf4e0c58341fa8
M1RRoN <mds.myst@gmail.com>
2023-05-06 11:56:55 +0400

    refactoring and delete send_massage

--- backend/users/backend/utils/message.py
+++ backend/users/backend/utils/message.py
@@ -1,42 +1,34 @@
-from abc import ABC
+from dataclasses import dataclass
+from typing import Any
 
-from utils.rabbitmq import RabbitMQ
 
-
-class BaseMessage(ABC):
-    def __init__(self, message: str, exchange_name: str, queue_name: str) -> None:
-        self.message = message
-        self.exchange_name = exchange_name
-        self.queue_name = queue_name
-
-    def send(self) -> None:
-        with RabbitMQ(
-                exchange_name=self.exchange_name,
-                queue_name=self.queue_name
-        ) as rabbitmq:
-            rabbitmq.publish(self.message)
+@dataclass
+class BaseMessage:
+    exchange_name: str
+    routing_key: str
+    message: Any
 
 
 class DevActivationMessage(BaseMessage):
-    def __init__(self, message: str) -> None:
-        super().__init__(message, 'dev_activation_exchange', 'dev_activation_queue')
+    exchange_name = 'dev_activation_exchange'
+    routing_key = 'dev_activation_queue'
 
 
 class ClientActivationMessage(BaseMessage):
-    def __init__(self, message: str) -> None:
-        super().__init__(message, 'client_activation_exchange', 'client_activation_queue')
+    exchange_name = 'client_activation_exchange'
+    routing_key = 'client_activation_queue'
 
 
 class OwnerAccessMessage(BaseMessage):
-    def __init__(self, message: str) -> None:
-        super().__init__(message, 'owner_access_exchange', 'owner_access_queue')
+    exchange_name = 'owner_access_exchange'
+    routing_key = 'owner_access_queue'
 
 
 class DevAccessMessage(BaseMessage):
-    def __init__(self, message: str) -> None:
-        super().__init__(message, 'dev_access_exchange', 'dev_access_queue')
+    exchange_name = 'dev_access_exchange'
+    routing_key = 'dev_access_queue'
 
 
 class FriendAddedMessage(BaseMessage):
-    def __init__(self, message: str) -> None:
-        super().__init__(message, 'friend_added_exchange', 'friend_added_queue')
+    exchange_name = 'friend_added_exchange'
+    routing_key = 'friend_added_queue'
--- backend/users/backend/utils/rabbitmq.py
+++ backend/users/backend/utils/rabbitmq.py
@@ -1,10 +1,11 @@
 import pika
 
 from config.settings.rabbitmq import RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_USERNAME, RABBITMQ_PASSWORD
+from utils.message import BaseMessage
 
 
 class RabbitMQ:
-    def __init__(self, exchange_name, queue_name):
+    def __init__(self, exchange_name: str, queue_name: str):
         self.host = RABBITMQ_HOST
         self.port = RABBITMQ_PORT
         self.username = RABBITMQ_USERNAME
@@ -12,6 +13,7 @@ class RabbitMQ:
         self.exchange_name = exchange_name
         self.queue_name = queue_name
 
+    def __enter__(self):
         credentials = pika.PlainCredentials(self.username, self.password)
         parameters = pika.ConnectionParameters(
             host=self.host,
@@ -21,10 +23,20 @@ class RabbitMQ:
         )
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
+        return self
 
-    def publish(self, message: str) -> None:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+    def send_message(self, message: BaseMessage) -> None:
         self.channel.basic_publish(
-            exchange=self.exchange_name,
-            routing_key=self.queue_name,
-            body=message.encode()
+            exchange=message.exchange_name,
+            routing_key=message.routing_key,
+            body=str(message.message).encode()
         )
+
+# Пример использования:
+# def publish():
+#     message = DevActivationMessage(message={'id': 1})
+#     with RabbitMQ(exchange_name=message.exchange_name, queue_name=message.routing_key) as client:
+#         client.send_message(message)
